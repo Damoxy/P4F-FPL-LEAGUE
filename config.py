9c977256fc3e0f76a80a019a48e201b37953053b
damola <12345678+username@users.noreply.github.com>
2025-05-23 17:11:53 +0100

    feat: add config

---
+++ config.py
@@ -0,0 +1,3 @@
+LEAGUE_ID = 937289
+FPL_LEAGUE_URL = f"https://fantasy.premierleague.com/api/leagues-classic/{LEAGUE_ID}/standings/"
+FPL_FIXTURES_URL = "https://fantasy.premierleague.com/api/fixtures/"
