21a503708112c031a5ce0adb4631d4517f683af4
Damola Obadeji <40131855+Damoxy@users.noreply.github.com>
2025-08-11 13:00:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 # League ID
-#LEAGUE_ID = 937289
+LEAGUE_ID = 610588
 FPL_LEAGUE_URL = f"https://fantasy.premierleague.com/api/leagues-classic/{LEAGUE_ID}/standings/"
 FPL_FIXTURES_URL = "https://fantasy.premierleague.com/api/fixtures/"
 
